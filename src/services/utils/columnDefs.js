34dba30830948985e230a363cf32ab30003e60ec
IvanProtsenko <ivnprotsenko@gmail.com>
2023-10-23 10:25:46 +0300

    feat: filter in progress

--- src/services/utils/columnDefs.js
+++ src/services/utils/columnDefs.js
@@ -1,4 +1,4 @@
-const LOCALE_VALUE = "en";
+const LOCALE_VALUE = 'en';
 
 const columnDefsAccounts = [
   {
@@ -61,7 +61,8 @@ const columnDefsAccounts = [
     filter: 'agNumberColumnFilter',
     editable: true,
     enableRowGroup: true,
-    valueFormatter: (param) => param.data.freezed_balance.toLocaleString(LOCALE_VALUE),
+    valueFormatter: (param) =>
+      param.data.freezed_balance.toLocaleString(LOCALE_VALUE),
   },
   {
     field: 'available_balance',
@@ -69,17 +70,21 @@ const columnDefsAccounts = [
     filter: 'agNumberColumnFilter',
     editable: true,
     enableRowGroup: true,
-    valueFormatter: (param) => param.data.available_balance.toLocaleString(LOCALE_VALUE),
+    valueFormatter: (param) =>
+      param.data.available_balance.toLocaleString(LOCALE_VALUE),
   },
   {
     headerName: 'Total balance',
     valueGetter: function sumField(params) {
-      return params.data.freezed_balance + params.data.available_balance
+      return params.data.freezed_balance + params.data.available_balance;
     },
     filter: 'agNumberColumnFilter',
     editable: true,
     enableRowGroup: true,
-    valueFormatter: (param) => (param.data.freezed_balance + param.data.available_balance).toLocaleString(LOCALE_VALUE),
+    valueFormatter: (param) =>
+      (
+        param.data.freezed_balance + param.data.available_balance
+      ).toLocaleString(LOCALE_VALUE),
   },
   {
     field: 'proxy.host',
@@ -109,22 +114,28 @@ const columnDefsAccounts = [
   },
   {
     valueGetter: function sumField(params) {
-      return params.data.objectives_progress ? params.data.objectives_progress.list : 0
+      return params.data.objectives_progress
+        ? params.data.objectives_progress.list
+        : 0;
     },
     headerName: 'Progress list',
+    filter: 'agNumberColumnFilter',
     suppressToolPanel: true,
     editable: true,
     enableRowGroup: true,
   },
   {
     valueGetter: function sumField(params) {
-      return params.data.objectives_progress ? params.data.objectives_progress.buy_now : 0
+      return params.data.objectives_progress
+        ? params.data.objectives_progress.buy_now
+        : 0;
     },
     headerName: 'Progress buy now',
+    filter: 'agNumberColumnFilter',
     suppressToolPanel: true,
     editable: true,
     enableRowGroup: true,
-  }
+  },
 ];
 
 export default columnDefsAccounts;
